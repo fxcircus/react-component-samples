09231eb00545fecff0dc5077aa0178e07ab88079
Roy Daniel <roydanielr@gmail.com>
2023-09-17 08:12:07 -0400

    start and stop timer

--- src/components/pomodoroTimer/PomodoroTimer.tsx
+++ src/components/pomodoroTimer/PomodoroTimer.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect  } from "react";
 import './pomodoroTimer.css'
 
 export default function PomodoroTimer () {
     const [ time, setTime ] = useState(1500)
+    const [ isCounting, setIsCounting ] = useState(false)
 
-    function secondsToMinutes(seconds) {
+    const secondsToMinutes = (seconds) => {
         const minutes = Math.floor(seconds / 60);
         const remainingSeconds = seconds % 60;
         
@@ -14,6 +15,31 @@ export default function PomodoroTimer () {
         
         return `${formattedMinutes}:${formattedSeconds}`;
     }
+
+    const toggleCountdown = () => {
+        setIsCounting(!isCounting); // Toggle the countdown
+    };
+    
+    useEffect(() => {
+        let intervalId;
+    
+        if (isCounting) {
+          intervalId = setInterval(() => {
+            if (time <= 0) {
+              clearInterval(intervalId);
+              setIsCounting(false);
+            } else {
+              setTime(time - 1);
+            }
+          }, 1000);
+        } else {
+          clearInterval(intervalId);
+        }
+    
+        return () => clearInterval(intervalId);
+      }, [isCounting, time]);
+
+
 
     return (
         <div className="pomodoro-timer-class">
@@ -24,15 +50,18 @@ export default function PomodoroTimer () {
                 <div className="pomodoro-backward-button">
                     <i className="fa fa-backward"></i>
                 </div>
-                <div className="pomodoro-play-pause-button">
-                    <i className="fa fa-play"></i>
+                <div className="pomodoro-play-pause-button" onClick={toggleCountdown}>
+                    { isCounting ? (
+                        <i className="fa fa-pause"></i>
+                    ): (
+                        <i className="fa fa-play"></i>
+                    )}
                 </div>
                 <div className="pomodoro-forward-button">
                     <i className="fa fa-forward"></i>
                 </div>
             </div>
-            
+
         </div>
     )
 }
-
