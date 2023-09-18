212dd807e2e2585c5afef7945241f9f484119e48
Roy Daniel <roydanielr@gmail.com>
2023-09-18 10:37:11 -0400

    Divider component

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Divider.css'; // Import the CSS file
+
+const Divider: React.FC = () => {
+  return <div className="divider"></div>; // Use the CSS class
+};
+
+export default Divider;
