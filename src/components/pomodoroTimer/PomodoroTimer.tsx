f8d1d0fb03da58d2aa44363e5d4071701a99d47d
Roy Daniel <roydanielr@gmail.com>
2023-09-18 07:52:24 -0400

    typescript syntax fix

--- src/components/pomodoroTimer/PomodoroTimer.tsx
+++ src/components/pomodoroTimer/PomodoroTimer.tsx
@@ -1,77 +1,80 @@
-import React from "react";
-import { useState, useEffect  } from "react";
+import React, { useState, useEffect } from "react";
 import './pomodoroTimer.css'
 
-export default function PomodoroTimer () {
-    const [ time, setTime ] = useState(1500)
-    const [ isCounting, setIsCounting ] = useState(false)
+export default function PomodoroTimer() {
+  const [time, setTime] = useState(1500);
+  const [isCounting, setIsCounting] = useState(false);
+  let intervalId: NodeJS.Timeout | undefined;
 
-    const secondsToMinutes = (seconds) => {
-        const minutes = Math.floor(seconds / 60);
-        const remainingSeconds = seconds % 60;
-        
-        const formattedMinutes = String(minutes).padStart(2, '0');
-        const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-        
-        return `${formattedMinutes}:${formattedSeconds}`;
-    }
+  const secondsToMinutes = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
 
-    const resetTimer = (interval) => {
-        setIsCounting(false)
-        setTime(interval)
-        return interval
-    }
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
 
-    const toggleCountdown = () => {
-        setIsCounting(!isCounting); // Toggle the countdown
-    };
+    return `${formattedMinutes}:${formattedSeconds}`;
+  }
 
-    const takeBreak = () => {
-        setTime(300)
-    }
-    
-    useEffect(() => {
-        let intervalId;
-    
-        if (isCounting) {
-          intervalId = setInterval(() => {
-            if (time <= 0) {
-              clearInterval(intervalId);
-              setIsCounting(false);
-            } else {
-              setTime(time - 1);
-            }
-          }, 1000);
+  const resetTimer = (interval: number) => {
+    setIsCounting(false);
+    setTime(interval);
+  }
+
+  const takeBreak = () => {
+    setTime(300);
+  }
+
+  const toggleCountdown = () => {
+    setIsCounting(!isCounting);
+  };
+
+  useEffect(() => {
+    if (isCounting) {
+      intervalId = setInterval(() => {
+        if (time <= 0) {
+          clearInterval(intervalId!);
+          setIsCounting(false);
         } else {
-          clearInterval(intervalId);
+          setTime(prevTime => prevTime - 1);
         }
-    
-        return () => clearInterval(intervalId);
-      }, [isCounting, time]);
+      }, 1000);
+    } else if (intervalId) {
+      clearInterval(intervalId);
+    }
 
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [isCounting, time]);
 
+  return (
+    <div className="pomodoro-timer-class">
 
-    return (
-        <div className="pomodoro-timer-class">
+      <div className="pomodoro-clock">{secondsToMinutes(time)}</div>
 
-            <div className="pomodoro-clock">{secondsToMinutes(time)}</div>
-            
-            <div className="pomodoro-button-area">
-                <button className="pomodoro-backward-button" onClick={(e) => {resetTimer(1500)}} >
-                    <i className="fas fa-undo"></i>
-                </button>
-                <button className="pomodoro-play-pause-area" onClick={toggleCountdown}>
-                    { isCounting ? (
-                        <i className="pomodoro-play-pause-button fas fa-pause-circle pause-circle-icon"></i>
-                    ): (
-                        <i className="pomodoro-play-pause-button fas fa-play-circle play-icon"></i>
-                    )}
-                </button>
-                <button className="pomodoro-break-button" onClick={takeBreak}>
-                    <i className="fas fa-coffee coffee-icon"></i>
-                </button>
-            </div>
+      <div className="pomodoro-button-area">
+        
+        <button className="pomodoro-backward-button" onClick={() => resetTimer(1500)} >
+          <i className="fas fa-undo"></i>
+        </button>
 
-        </div>
-    )
+        <button className="pomodoro-play-pause-area" onClick={toggleCountdown}>
+          {isCounting ? (
+            <i className="pomodoro-play-pause-button fas fa-pause-circle pause-circle-icon"></i>
+          ) : (
+            <i className="pomodoro-play-pause-button fas fa-play-circle play-icon"></i>
+          )}
+        </button>
+
+        <button className="pomodoro-break-button" onClick={takeBreak}>
+          <i className="fas fa-coffee coffee-icon"></i>
+        </button>
+        
+      </div>
+
+    </div>
+  )
 }
